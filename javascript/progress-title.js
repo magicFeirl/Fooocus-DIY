dc517d796b70b6d7f0f41f02e4137b3ae794e535
magicFeirl <2100709458@qq.com>
2025-06-01 20:22:15 +0800

    feat: 添加时间显示

--- javascript/progress-title.js
+++ javascript/progress-title.js
@@ -1,23 +1,34 @@
 onUiLoaded(() => {
     const title = document.title;
+    let startTime = 0
+
+    const hidden = () => {
+        document.title = title
+        startTime = 0
+    }
 
     const progressBar = document.querySelector("#progress-bar");
     const observe = new MutationObserver((record) => {
         if (progressBar.classList.contains('hidden')) {
-            document.title = title
-            return
+            return hidden()
         }
-        
+
+        if (!startTime) {
+            startTime = Date.now()
+        }
+
         const progressEl = progressBar.querySelector("progress");
         const currentProgress = progressEl.value;
         const progressText = document.querySelector(
             ".loader-container span"
         ).textContent;
+
         const match = progressText.match(/image (\d+)\/(\d+)/);
 
         if (match) {
             const [_, current, total] = match;
-            document.title = ` ${currentProgress}% | sampling ${current} / ${total}`;
+            const time = ((Date.now() - startTime) / 1000).toFixed(2)
+            document.title = ` ${currentProgress}% ${time}s | Sampling ${current} / ${total}`;
         }
     });
 
